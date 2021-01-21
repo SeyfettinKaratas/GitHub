2262853e2e642b685147379993da007b5f459a55
SeyfettinKaratas <skaratas@outlook.com.tr>
2021-01-21 19:16:55 +0300

    ödev dosyası

---
+++ MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle ( Musteri musteri )
+        {
+            Console.WriteLine("Musteri ekleme:" + Environment.NewLine+musteri.Id+ " " +musteri.Adi +" "+ musteri.Soyadi);
+        }
+
+        public void MusteriListele (Musteri[] musteriler)
+        {
+            Console.WriteLine("Musteri Listele:");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id+" "+musteri.Adi+" "+musteri.Soyadi);
+                
+            }
+        }
+
+        public void MusteriSil ( Musteri musteri )
+        {
+            Console.WriteLine("Musteri sil:" + Environment.NewLine + musteri.Id+ "  " + "  "+ musteri.Adi+ "  " + "  "+ musteri.Soyadi);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 12345;
+            musteri1.Adi = "Seyfettin";
+            musteri1.Soyadi = "Karatas";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 23456;
+            musteri2.Adi = "Burhan";
+            musteri2.Soyadi = "Savci";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+           
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+
+
+
+            MusteriManager musteriManager1 = new MusteriManager();
+            musteriManager1.MusteriListele(musteriler);
+          
+
+
+
+            MusteriManager musteriManager2 = new MusteriManager();
+            musteriManager2.MusteriSil(musteri1);
+            musteriManager2.MusteriSil(musteri2);
+
+
+
+
+        }
+    }
+}
